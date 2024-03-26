cdf3c1a11315e50bffd82d5a0e47fb6c8ad69275
omar ahmed <omar01159361955@gmail.com>
2024-03-27 01:24:27 +0200

    test

--- obj/Debug/net8.0/InstitueProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/InstitueProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InstitueProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2617a87e3eaa7b64fc2737fa4633206fe4e126a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("InstitueProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InstitueProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
